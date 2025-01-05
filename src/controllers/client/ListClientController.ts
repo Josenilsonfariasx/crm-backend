cfef2b09e012fe5dbe1fd6807fcdea3de31e89d3
¨josenilsonfariasx¨ <fariaslwork@gmail.com>
2025-01-05 07:22:54 -0300

    feat: adding pagination in clients

--- src/controllers/client/ListClientController.ts
+++ src/controllers/client/ListClientController.ts
@@ -2,6 +2,9 @@ import { Request, Response } from "express";
 import { listClientService } from "../../services/client/listClientService";
 
 export const ListClientController = async (req: Request, res: Response) => {
-  const clients = await listClientService();
+  const page = parseInt(req.query.page as string) || 1;
+  const limit = parseInt(req.query.limit as string) || 10;
+
+  const clients = await listClientService(page, limit);
   return res.json(clients);
-}; 
+};
--- src/services/client/listClientService.ts
+++ src/services/client/listClientService.ts
@@ -1,7 +1,9 @@
 import prismaClient from "../../prisma";
 
-export const listClientService = async () => {
+export const listClientService = async (page: number = 1, limit: number = 10) => {
   const clients = await prismaClient.client.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
     select: {
       id: true,
       name: true,
@@ -12,5 +14,13 @@ export const listClientService = async () => {
     },
   });
 
-  return clients;
+  const totalClients = await prismaClient.client.count();
+  const totalPages = Math.ceil(totalClients / limit);
+
+  return {
+    clients,
+    totalClients,
+    totalPages,
+    currentPage: page,
+  };
 };
